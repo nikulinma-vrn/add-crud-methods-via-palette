f302cf9892b9d843c3c6755b46555f0da3e4ffbe
maksim <m.nikulin@haulmont.com>
2024-06-14 10:05:53 +0300

    INIT. Entity was created

---
+++ src/main/java/com/example/crudresttest/CrudresttestApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudresttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudresttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudresttestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/crudresttest/entity/User.java
@@ -0,0 +1,77 @@
+package com.example.crudresttest.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "user_")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "credit_card")
+    private String creditCard;
+
+    public String getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(String creditCard) {
+        this.creditCard = creditCard;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
