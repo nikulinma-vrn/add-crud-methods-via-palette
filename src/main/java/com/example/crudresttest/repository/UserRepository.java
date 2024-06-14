19daceff099d193abb381980585cd59f588e1e21
maksim <m.nikulin@haulmont.com>
2024-06-14 10:06:51 +0300

    Repository was created

---
+++ src/main/java/com/example/crudresttest/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.crudresttest.repository;
+
+import com.example.crudresttest.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
+}
