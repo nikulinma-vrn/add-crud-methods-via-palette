6d6e182d3a06de9ebb9dc3dbac25e987b2c9e7a4
m.nikulin <m.nikulin@haulmont.com>
2024-06-18 12:19:23 +0300

    Todo was created in UserController

--- src/main/java/com/example/crudresttest/api/v1/UserController.java
+++ src/main/java/com/example/crudresttest/api/v1/UserController.java
@@ -35,71 +35,41 @@ public class UserController {
         this.objectMapper = objectMapper;
     }
 
-    @GetMapping
-    public List<User> getList() {
-        return userRepository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public User getOne(@PathVariable UUID id) {
-        Optional<User> userOptional = userRepository.findById(id);
-        return userOptional.orElseThrow(() ->
-                new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity with id `%s` not found".formatted(id)));
-    }
-
-    @GetMapping("/by-ids")
-    public List<User> getMany(@RequestParam List<UUID> ids) {
-        return userRepository.findAllById(ids);
-    }
-
-    @PostMapping
-    public User create(@RequestBody User user) {
-        return userRepository.save(user);
-    }
-
-    @PostMapping("/bulk")
-    public List<User> createMany(@RequestBody List<User> users) {
-        return userRepository.saveAll(users);
-    }
-
-    @PatchMapping("/{id}")
-    public User patch(@PathVariable UUID id, @RequestBody JsonNode patchNode) throws IOException {
-        User user = userRepository.findById(id).orElseThrow(() ->
-                new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity with id `%s` not found".formatted(id)));
-
-        objectMapper.readerForUpdating(user).readValue(patchNode);
-
-        return userRepository.save(user);
-    }
-
+    /*
+     * ToDo: Amplicode Designer -> Request Handling -> Get List
+     *       DTO class: User
+     *       Pagination: OFF
+     *
+     * ToDo: Amplicode Designer -> Request Handling -> Get One
+     *       DTO class: User
+     *       Resource path: /{id}
+     *
+     * ToDo: Amplicode Designer -> Request Handling -> Get Many
+     *       DTO class: User
+     *       Resource path: /by-ids
+     *
+     * ToDo: Amplicode Designer -> Request Handling -> Create
+     *       DTO class: User
+     *
+     * ToDo: Amplicode Designer -> Request Handling -> CreateMany
+     *       DTO class: User
+     *       Resource path: /bulk
+     *
+     * ToDo: Amplicode Designer -> Request Handling -> Path
+     *       DTO class: User
+     *       Resource path: /{id}
+     *
+     * ToDo: Amplicode Designer -> Request Handling -> PathMany
+     *       DTO class: User
+     *
+     * ToDo: Amplicode Designer -> Request Handling -> Delete
+     *       DTO class: User
+     *       Resource path: /{id}
+     *
+     * ToDo: Amplicode Designer -> Request Handling -> DeleteMany
+     *       DTO class: User
+     *
+     */
 
 
-    @DeleteMapping("/{id}")
-    public User delete(@PathVariable UUID id) {
-        User user = userRepository.findById(id).orElse(null);
-        if (user != null) {
-            userRepository.delete(user);
-        }
-        return user;
-    }
-
-    @DeleteMapping
-    public void deleteMany(@RequestParam List<UUID> ids) {
-        userRepository.deleteAllById(ids);
-    }
-
-    @PatchMapping
-    public List<UUID> patchMany(@RequestParam List<UUID> ids, @RequestBody JsonNode patchNode) throws IOException {
-        Collection<User> users = userRepository.findAllById(ids);
-
-        for (User user : users) {
-//            objectMapper.readerForUpdating(user).readValue(patchNode);
-            objectMapper.readerForUpdating(user).readValue(patchNode);
-        }
-
-        List<User> resultUsers = userRepository.saveAll(users);
-        return resultUsers.stream()
-                .map(User::getId)
-                .toList();
-    }
 }
